0cd97a2f3ef21f3a22163b4539f8509450fc9442
“Joe <joe_simmons_99@yahoo.com>
2022-01-31 22:03:24 -0800

    added aave remappings compiled but with warnings. 9hr 15min mark

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -10,7 +10,7 @@ def get_weth():
     """
     account = get_account()
     weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
-    tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18, "required_confs":0})
+    tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
     tx.wait(2)
     print("Received 0.1 WETH")
     return tx
